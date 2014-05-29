5f5e1e6df035a1e98df384cde545093184c41be8
wk00121 <727009440@qq.com>
2014-05-29 13:33:14 +0800

    1.7

---
+++ src/app/sample/streetlocation/SettingActivity.java
@@ -0,0 +1,32 @@
+package app.sample.streetlocation;
+
+import android.preference.PreferenceActivity;
+
+public class SettingActivity extends PreferenceActivity{
+	/**
+	 * 定位开关CheckBoxPreference key
+	 */
+	private String locationSwitchPrefKey;
+
+	/**
+	 * 扫描间隔ListPreference key
+	 */
+	private String scanIntervalPrefKey;
+
+	/**
+	 * 删除地图数据key
+	 */
+	private String deleteMapDataKey;
+
+	/**
+	 * 語言選擇key
+	 */
+	private String languageKey;
+	
+	/**
+	 * 显示模式key
+	 */
+	private String showModeKey;
+	
+	
+}
